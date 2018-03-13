9989cf401d70018c490831f4a10f345fabba583e
augustocollerone <collerone.augusto@gmail.com>
2018-03-13 16:27:42 -0300

    Pods added

--- MercadoPagoSDK/Pods/Target Support Files/Pods-MercadoPagoSDKTests/Pods-MercadoPagoSDKTests-frameworks.sh
+++ MercadoPagoSDK/Pods/Target Support Files/Pods-MercadoPagoSDKTests/Pods-MercadoPagoSDKTests-frameworks.sh
@@ -136,10 +136,12 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/MercadoPagoPXTracking/MercadoPagoPXTracking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MercadoPagoServices/MercadoPagoServices.framework"
+  install_framework "${PODS_ROOT}/PayCardsRecognizer/Source/PayCardsRecognizer.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/MercadoPagoPXTracking/MercadoPagoPXTracking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MercadoPagoServices/MercadoPagoServices.framework"
+  install_framework "${PODS_ROOT}/PayCardsRecognizer/Source/PayCardsRecognizer.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
