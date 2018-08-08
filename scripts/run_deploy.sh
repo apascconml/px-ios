8b6b8676a625f5d316ea2be9b3a29ed2dff993f5
Matias Casanova <maty.casanova@gmail.com>
2018-08-08 19:20:13 -0300

    updated bundle path on fastlane [ci deploy]

--- scripts/run_deploy.sh
+++ scripts/run_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-bundle install --gemfile=.fastlane/Gemfile 
+bundle install --gemfile=.fastlane/Gemfile --path .bundle
 BUNDLE_GEMFILE=.fastlane/Gemfile bundle exec fastlane start_deploy
